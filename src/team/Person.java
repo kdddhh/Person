c46b7d4b8666c0e6829695e401ecbf25f65fbd40
DongHyun <ehdgus6709@naver.com>
2023-02-22 22:02:02 +0900

    first commit

---
+++ src/team/Person.java
@@ -0,0 +1,15 @@
+package team;
+
+public class Person {
+	String name;
+	int age;
+	String gender;
+	int stature;
+	
+	public Person(String name, int age, String gender, int stature) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.stature = stature;
+	}
+}
